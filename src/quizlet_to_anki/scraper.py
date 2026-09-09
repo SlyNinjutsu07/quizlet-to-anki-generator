adf6b0efc860095811d1fa9d24f6ea1ee05e6c86
Sly <85970858+SlyNinjutsu07@users.noreply.github.com>
2026-09-09 14:08:21 -0400

    add sample set to test dir; next_data json search in scraper.py

--- src/quizlet_to_anki/scraper.py
+++ src/quizlet_to_anki/scraper.py
@@ -1,7 +1,16 @@
 """Reads a Quizlet set and generates a list of Cards."""
 
+from models import Card
+
+import json
 import requests
+import sys
 from bs4 import BeautifulSoup
+
+# return list[Card]
+def extract_cards(next_data: dict) -> None:
+
+    pass
 
 # get url from user (handled by cli.py)
 url = "https://quizlet.com"
@@ -10,10 +19,37 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 
-response = requests.get(url, headers=headers)
+# Decides whether the program gets to run or not
+try:
+    response = requests.get(url)
 
-if response.status_code == 200:
-    #do the scrape
-    pass
+    response.raise_for_status()
+    # if above returns no error...
+    print("Success! Parsing page...")
+
+except requests.exceptions.HTTPError as error:
+    print(f"HTTP error occurred: {error}")
+    sys.exit(1)
+except Exception as error:
+    print(f"An unexpected error occurred: {error}")
+    sys.exit(1)
+
+# locate the __NEXT_DATA__ json
+soup = BeautifulSoup(response.text, "html.parser")
+next_data_tag = soup.find("script", id="__NEXT_DATA__")
+
+# check if the json data exists
+if next_data_tag and next_data_tag.string:
+    next_data_json = json.loads(next_data_tag.string)
+
+    try:
+        set_data = next_data_json["props"]["pageProps"]["dehydratedState"]["queries"][0][
+            "state"
+        ]["data"]
+        print(json.dumps(set_data, indent=2))
+    except KeyError:
+        print("The expected JSON path was not found in \'__NEXT_DATA__\'.")
 else:
-    print(f"Failed to load page. status code: {response.status_code}")
+  print("\'__NEXT_DATA__\' script tag not found on this page.")
+
+
