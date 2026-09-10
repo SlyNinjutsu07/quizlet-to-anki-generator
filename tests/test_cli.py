16352bbb050d139d47d012918d0f8f6403bfc50b
Sly-UbuntuWindowsTerminal <85970858+SlyNinjutsu07@users.noreply.github.com>
2026-09-10 18:56:58 -0400

    add more potential test cases

---
+++ tests/test_cli.py
@@ -0,0 +1,12 @@
+# functions to check if a URL is valid first and can be read properly
+def test_url(url):
+    pass
+
+def test_next_data_exists(): #return true or false
+    pass
+
+def test_scraper():
+    pass
+
+def test_converter():
+    pass
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,5 +1,12 @@
-def test_extractor():
+# Test scraper functionality
+def test_extract_next_data():
     pass
 
 def test_extract_cards():
     pass
+
+def test_redux_to_json():
+    pass
+
+
+
